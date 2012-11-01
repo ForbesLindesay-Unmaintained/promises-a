7e9bc8952f6799e0b494bbffffbba46f73105357
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-11-01 10:58:44 +0000

    Rewmove un-necessary semi-colons

--- index.js
+++ index.js
@@ -1,72 +1,76 @@
-(function () {
+;(function () {
   function promise() {
     var resolved = false,
         fulfilled = false,
         val,
-        waiting = [], running = false;
+        waiting = [],
+        running = false
 
     function next() {
       if (waiting.length) {
-        running = true;
-        waiting.shift()();
+        running = true
+        waiting.shift()()
       } else {
-        running = false;
+        running = false
       }
     }
     function then(cb, eb, pb) {
-      var def = promise();
+      var def = promise()
       function done() {
-        var callback = fulfilled ? cb : eb;
+        var callback = fulfilled ? cb : eb
         if (callback) {
           setTimeout(function () {
             var value;
             try {
-              value = callback(val);
+              value = callback(val)
             } catch (ex) {
-              def.reject(ex);
-              return next();
+              def.reject(ex)
+              return next()
             }
             def.fulfill(value);
             next();
-          }, 0);
+          }, 0)
         } else if (fulfilled) {
-          def.fulfill(val);
-          next();
+          def.fulfill(val)
+          next()
         } else {
-          def.reject(val);
-          next();
+          def.reject(val)
+          next()
         }
       }
       waiting.push(done);
       if (resolved && !running) {
-        next();
+        next()
       }
-      return def.promise;
+      return def.promise
     }
     function resolve(success, value) {
       if (resolved) return;
       if (success && typeof value === 'object' && typeof value.then === 'function') {
-        value.then(fulfill, reject);
-        return;
+        value.then(fulfill, reject)
+        return
       }
-      resolved = true;
-      fulfilled = success;
-      val = value;
-      next();
+      resolved = true
+      fulfilled = success
+      val = value
+      next()
     }
     function fulfill(val) {
-      resolve(true, val);
+      resolve(true, val)
     }
     function reject(err) {
-      resolve(false, err);
+      resolve(false, err)
     }
 
-    return {promise: {then: then}, fulfill: fulfill, reject: reject};
-  };
-  
-  if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
-    module.exports = promise;
-  } else {
-    window.promise = promise;
+    return {
+      promise: {then: then},
+      fulfill: fulfill,
+      reject: reject
+    }
   }
-}());
+  
+  if (typeof module != 'undefined' && typeof module.exports != 'undefined')
+    module.exports = promise
+  else
+    window.promise = promise
+}())
